6b0dee39715001419923007c4cad9460232a504e
Dragos Dehelean <dragos.dehelean@selenis.ro>
2019-02-15 04:08:08 +0200

    Updates

---
+++ precache-manifest.d9bcd3a3583bdb2a804ec350203c72df.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = [
+  {
+    "revision": "d5921203f37757cf4baf",
+    "url": "/React-test-app/static/js/runtime~main.d5921203.js"
+  },
+  {
+    "revision": "f2acc3119a7f513a14f8",
+    "url": "/React-test-app/static/js/main.f2acc311.chunk.js"
+  },
+  {
+    "revision": "b15e085b6c69950cc522",
+    "url": "/React-test-app/static/js/2.b15e085b.chunk.js"
+  },
+  {
+    "revision": "3369e0d0b86cf6c4be6ca3eb2169527b",
+    "url": "/React-test-app/index.html"
+  }
+];
